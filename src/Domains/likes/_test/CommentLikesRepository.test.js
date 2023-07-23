19a09c66700dc225abd16e4a33e05355cd563d7d
wisudantara <lulus.wisudantara.m@gmail.com>
2023-07-24 01:50:34 +0200

    add comment likes test case

---
+++ src/Domains/likes/_test/CommentLikesRepository.test.js
@@ -0,0 +1,13 @@
+const CommentLikesRepository = require('../CommentLikesRepository');
+
+describe('A CommentLikesRepository interace', () => {
+  it('should throw error when invoke abstract behaviour', async () => {
+    // Arrange
+    const commentLikesRepository = new CommentLikesRepository();
+
+    // Action and Assert
+    await expect(commentLikesRepository.checkCommentLike({})).rejects.toThrowError('COMMENT_LIKES_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentLikesRepository.putCommentLike({})).rejects.toThrowError('COMMENT_LIKES_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentLikesRepository.putCommentUnlike({})).rejects.toThrowError('COMMENT_LIKES_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
